0c67307571287a4769937e472c91c1ad5d94be81
Aymene Haloui <ahaloui@e2r12p2.1337.ma>
2023-03-02 15:00:14 +0100

    push

--- sort_functions.c
+++ sort_functions.c
@@ -6,7 +6,7 @@
 /*   By: ahaloui <ahaloui@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/24 12:53:15 by ahaloui           #+#    #+#             */
-/*   Updated: 2023/03/01 20:23:10 by ahaloui          ###   ########.fr       */
+/*   Updated: 2023/03/02 14:55:46 by ahaloui          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,6 +51,42 @@ void push_to_B(t_list **head_a, t_list **head_b, int chunk)
     }
 }
 
+int calculate_chunk(int size_list)
+{
+    int chunk;
+
+    chunk = 0;
+    while (size_list > 0)
+    {
+        size_list = size_list / 2;
+        chunk++;
+    }
+    return (chunk);
+}
+
+
+// int instriction(int size_list, int position)
+// {
+//     int i;
+//     int j;
+
+//     i = 0;
+//     j = 0;
+//     while (i < size_list)
+//     {
+//         if (i == position)
+//             return (j);
+//         i++;
+//         j++;
+//         if (i == size_list)
+//         {
+//             i = 0;
+//             j++;
+//         }
+//     }
+//     return (0);
+// }
+
 void f1(t_list **head_a, t_list **head_b, int size_b)
 {
     while (get_position(*head_b, size_b - 2) != 0 && get_position(*head_b, size_b - 2) >= size_b / 2)
@@ -83,36 +119,36 @@ void f2(t_list **head_a, t_list **head_b, int size_b)
 void sort_numbers(t_list **head_a, t_list **head_b, int chunk_divide)
 {
     int chunk;
-	// int	instriction_max;
-	// int	instriction_bef_max;
-    // int middle;
-    // int size_b;
+	int	instriction_max;
+	int	instriction_bef_max;
+    int middle;
+    int size_b;
 
     
     chunk = chunk_divide;
     // print_list(*head_a);
     push_to_B(head_a, head_b, chunk);
     // print_list(*head_b);
-    // size_b = ft_lstsize(*head_b);
-    // while (size_b)
-    // {
-	// 	size_b = ft_lstsize(*head_b);
-    // //     // // printf("pos = [%d]\n", get_position(*head_a, ft_lstsize(*head_a)) - 1);
-	// 	instriction_max = instriction(size_b, get_position(*head_a, ft_lstsize(*head_a)) - 1);
-	// 	instriction_bef_max = instriction(size_b, get_position(*head_a, ft_lstsize(*head_a) - 2));
-    //     middle = (size_b / 2);
-    //     // printf("instriction_max = %d > ", instriction_max);
-    //     // printf("instriction_bef_max = %d\n", instriction_bef_max);
-    //     if (instriction_max > instriction_bef_max)
-    //         f1(head_a, head_b, size_b);
-	// 	else
-    //     	f2(head_a, head_b, size_b);
-    //     size_b--;
-    // }
+    size_b = ft_lstsize(*head_b);
+	instriction_max = instriction(size_b, get_position(*head_a, ft_lstsize(*head_a)) - 1);
+	instriction_bef_max = instriction(size_b, get_position(*head_a, ft_lstsize(*head_a) - 2));
+    while (size_b)
+    {
+		size_b = ft_lstsize(*head_b);
+    //     // // printf("pos = [%d]\n", get_position(*head_a, ft_lstsize(*head_a)) - 1);
+        middle = (size_b / 2);
+        // printf("instriction_max = %d > ", instriction_max);
+        // printf("instriction_bef_max = %d\n", instriction_bef_max);
+        if (instriction_max > instriction_bef_max)
+            f1(head_a, head_b, size_b);
+		else
+        	f2(head_a, head_b, size_b);
+        size_b--;
+    }
 }
 
 
-// Function to calculate numbre of instriction
+//Function to calculate numbre of instriction
 int instriction(int size_b, int pos)
 {
     int middle;
@@ -125,6 +161,8 @@ int instriction(int size_b, int pos)
         instriction = pos + 1;
     return (instriction);
 }
+
+
 
 void sort(t_list **head_a, t_list **head_b)
 {
@@ -142,6 +180,9 @@ void sort(t_list **head_a, t_list **head_b)
     else if (size > 5 && size <= 100)
         sort_numbers(head_a, head_b, 5);
     else if (size > 100)
+    {
         sort_numbers(head_a, head_b, 8);
+        //sort_500(head_a, head_b);
+    }
 }
 
